8e2f0d1119271e7c9a27368fd9695ad9b1568698
Tobey Blaber <tobey@straitjacket.software>
2021-05-25 02:05:46 +0100

    Initial commit

---
+++ Straitjacket.Math/Math.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Straitjacket
+{
+    public static class Math
+    {
+        public static float RoundToNearest(float x, float y) => y * Mathf.Round(x / y);
+        public static double RoundToNearest(double x, double y) => y * Math.Round(x / y);
+
+        public static float FloorToNearest(float x, float y) => y * Mathf.Floor(x / y);
+        public static double FloorToNearest(double x, double y) => y * Math.Floor(x / y);
+    }
+}
