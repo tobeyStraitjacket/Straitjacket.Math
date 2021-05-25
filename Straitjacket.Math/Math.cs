fd9aa993ff455b33ca74d44fb34b3f7872f2d3dc
Tobey Blaber <tobey@straitjacket.software>
2021-05-25 03:04:27 +0100

    Reference the `System.Math` class explicitly.

--- Straitjacket.Math/Math.cs
+++ Straitjacket.Math/Math.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Straitjacket
 {
     public static class Math
     {
         public static float RoundToNearest(float x, float y) => y * Mathf.Round(x / y);
-        public static double RoundToNearest(double x, double y) => y * Math.Round(x / y);
+        public static double RoundToNearest(double x, double y) => y * System.Math.Round(x / y);
 
         public static float FloorToNearest(float x, float y) => y * Mathf.Floor(x / y);
-        public static double FloorToNearest(double x, double y) => y * Math.Floor(x / y);
+        public static double FloorToNearest(double x, double y) => y * System.Math.Floor(x / y);
     }
 }
