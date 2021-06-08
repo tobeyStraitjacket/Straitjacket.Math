b213f58c12f9ed92439d9a1e43ed044e5431e0b7
Tobey Blaber <tobey@straitjacket.software>
2021-06-08 23:36:47 +0100

    Enable extension methods usage

--- Straitjacket.Math/Math.cs
+++ Straitjacket.Math/Math.cs
@@ -4,10 +4,10 @@ namespace Straitjacket
 {
     public static class Math
     {
-        public static float RoundToNearest(float x, float y) => y * Mathf.Round(x / y);
-        public static double RoundToNearest(double x, double y) => y * System.Math.Round(x / y);
+        public static float RoundToNearest(this float num, float factor) => factor * Mathf.Round(num / factor);
+        public static double RoundToNearest(this double num, double factor) => factor * System.Math.Round(num / factor);
 
-        public static float FloorToNearest(float x, float y) => y * Mathf.Floor(x / y);
-        public static double FloorToNearest(double x, double y) => y * System.Math.Floor(x / y);
+        public static float FloorToNearest(this float num, float factor) => factor * Mathf.Floor(num / factor);
+        public static double FloorToNearest(this double num, double factor) => factor * System.Math.Floor(num / factor);
     }
 }
