0b5a724c98fd3c8e2a7c2d6f3d5176ce12a7fd4b
Tobey Blaber <tobey@straitjacket.software>
2021-06-08 23:37:19 +0100

    Add Map() function which maps an input number from one range into another.

--- Straitjacket.Math/Math.cs
+++ Straitjacket.Math/Math.cs
@@ -9,5 +9,27 @@ namespace Straitjacket
 
         public static float FloorToNearest(this float num, float factor) => factor * Mathf.Floor(num / factor);
         public static double FloorToNearest(this double num, double factor) => factor * System.Math.Floor(num / factor);
+
+        public static float Map(this float input,
+                                float inputFrom, float inputTo,
+                                float outputFrom, float outputTo)
+        {
+            float delta = Mathf.InverseLerp(inputFrom, inputTo, input);
+            return Mathf.Lerp(outputFrom, outputTo, delta);
+        }
+
+        public static double Map(this double input,
+                                 double inputFrom, double inputTo,
+                                 double outputFrom, float outputTo)
+        {   // using y = mx + c to solve where:
+            // m = slope of the line (ratio of range2 / range1)
+            // c = the point where y intercepts the line (difference between range2 and range1 start/end, multiplied by the slope)
+            // y = dependent (output) variable
+            // x = independent (input) variable
+
+            double slope = (outputTo - outputFrom) / (inputFrom / inputTo);
+            double intercept = outputFrom - slope * inputFrom; // also equals outputTo - slope * inputTo
+            return slope * input + intercept;
+        }
     }
 }
