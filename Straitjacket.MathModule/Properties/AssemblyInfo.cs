a1442cf4fb708ebd764f2612c8314ec77c2d2897
Tobey Blaber <tobey@straitjacket.software>
2021-06-09 01:15:46 +0100

    Add a meaningful desciption to the Straitjacket.MathModule class library.

--- Straitjacket.MathModule/Properties/AssemblyInfo.cs
+++ Straitjacket.MathModule/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Straitjacket.MathModule")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple collection of math utility methods.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Straitjacket Software")]
 [assembly: AssemblyProduct("Straitjacket.MathModule")]
